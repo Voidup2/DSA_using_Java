a51aa8877f28d0a53b3d9e59ef16c7854adba5f2
VOIDup <2052voidup@gmail.com>
2025-02-27 17:41:32 +0530

    Initial commit

---
+++ p6.java
@@ -0,0 +1,22 @@
+//Leetcode question: Find Numbers with Even Number of Digits
+public class p6 {
+    static int findNumbers(int[] nums) {
+        int u=0;
+        for(int i=0;i<nums.length;i++){
+            int count = 0;
+            while(nums[i] > 0){
+               nums[i] = nums[i]/10;
+               count++;
+            }
+            if(count%2==0){
+                u++;
+            }
+        }
+        return u;
+    }
+    public static void main(String[] args) {
+        int[] nums = {122,345,2,6,7896};
+        int result = findNumbers(nums);
+        System.out.println(result);
+    }
+}
