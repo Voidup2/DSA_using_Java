e92bef1945af127acc06fbab2d820e37a4dd5a90
VOIDup <2052voidup@gmail.com>
2025-03-14 20:44:31 +0530

    Optimized the code

--- p10.java
+++ p10.java
@@ -1,19 +1,24 @@
 //Leetcode Question: Search a 2D array
 public class p10 {
     public static boolean searchMatrix(int[][] matrix, int target) {
-        int r=matrix.length;
-        for(int i=0;i<r;i++){
-            for(int j=0;j<matrix[i].length;j++){
-                if(matrix[i][j]==target){
-                    return true;
-                }
+        int r = 0;
+        int c = matrix[r].length-1;
+        while(r < matrix.length && c>=0){
+            if(target < matrix[r][c]){
+                c--;
+            }
+            else if(target > matrix[r][c]){
+                r++;
+            }
+            else if(target == matrix[r][c]){
+                return true;
             }
         }
         return false;
     }
     public static void main(String[] args) {
         int[][] a = {{1,3,5,7},{10,11,16,20},{23,30,34,60}};
-        boolean result = searchMatrix(a,31);
+        boolean result = searchMatrix(a,60);
         System.out.println(result);
     }
 }
