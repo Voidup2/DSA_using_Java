19d3f2daf5a4bef9918d01e30b08fa3d19852444
VOIDup <2052voidup@gmail.com>
2025-03-20 23:14:08 +0530

    Gave the necessary comments

--- p21.java
+++ p21.java
@@ -14,10 +14,10 @@ public class p21 {
     public static void cycleSort(int[] arr){
         int i=0;
         while(i<arr.length){
-            int ci = arr[i] - 1;
-            if(arr[i] != arr[ci]){
-                int temp = arr[ci];
-                arr[ci] = arr[i];
+            int ci = arr[i] - 1;//Initializing the correct index after each iteration is good because it solves the problem of getting in infinite loop with duplicate elements,
+            if(arr[i] != arr[ci]){//since while checking the condition previously if(arr[i] != i+1) the duplicate element at wrong index is swapped with same element at the correct positon 
+                int temp = arr[ci];// so it will go in infinite loop to avoid that we must use correct index which is initialized at the start of each iteration and checks the value at i and at correct index 
+                arr[ci] = arr[i];//for that element if both values are same the check condition if(arr[i] != arr[ci]) will not run and the else condition will work so the i is incremented and the program proceeds
                 arr[i] = temp;
             }
             else{
