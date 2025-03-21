b85776ee12d9a8c113c9d4d47bfaeb59a078c6fe
VOIDup <2052voidup@gmail.com>
2025-03-22 00:32:56 +0530

    Leetcode question related to cycle sort

---
+++ p22.java
@@ -0,0 +1,35 @@
+//
+public class p22 {
+    public static void cycleSort(int[] arr){
+        int i=0;
+        while(i<arr.length){
+            int ci = arr[i]-1;
+            if(arr[i] != arr[ci]){
+                int temp = arr[ci];
+                arr[ci] = arr[i];
+                arr[i] = temp;
+            }
+            else{
+                i++;
+            }
+        }
+    } 
+    public static int findDuplicate(int[] nums) {
+        cycleSort(nums);
+        int i=0;
+        while(i<nums.length){
+            if(nums[i] != i+1){
+                return nums[i];
+            }
+            else{
+                i++;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int[] arr = {2,1,5,4,4};
+        int result = findDuplicate(arr);
+        System.out.println(result);
+    }
+}
---
+++ p23.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class p23 {
+    public static List<Integer> findDuplicates(int[] nums) {
+        cycleSort(nums);
+        List<Integer> result = new ArrayList<>();
+        for(int i=0;i<nums.length;i++){
+            if(nums[i] != i+1){
+                result.add(nums[i]);
+            }
+        }
+        return result;
+    }
+    public static void cycleSort(int[] arr){
+        int i=0;
+        while(i<arr.length){
+            int ci = arr[i]-1;
+            if(arr[i] != arr[ci]){
+                int temp = arr[ci];
+                arr[ci] = arr[i];
+                arr[i] = temp;
+            }
+            else{
+                i++;
+            }
+        }
+    }
+    public static void main(String[] args) {
+        int[] arr = {4,3,2,7,8,2,3,1};
+        List<Integer> result = findDuplicates(arr);
+        System.out.println(result);
+    }
+}
